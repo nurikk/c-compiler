7d3c2eb088c2d4bc06ce2a553ede60b36e4b34ec
Ainur Timerbaev (Lazada Group) <ainur.timerbaev@lazada.com>
2018-06-19 15:20:13 +0800

    rewrite tokenizator

--- src/main/kotlin/compiler/Tokenizer.kt
+++ src/main/kotlin/compiler/Tokenizer.kt
@@ -1,69 +1,99 @@
 package compiler
 
 
-import java.util.regex.Pattern
+import java.lang.Character.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class Tokenizer {
-    private val tokenInfos = ArrayList<TokenInfo>()
+
     private var tokens = ArrayList<Token>()
+    private var lastChar = ' '
+
+    private val singleCharTokens = Arrays.asList('(', ')', '{', '}', '*', '/', '<', '>', ';', '=', '-', '+', '~', ',')
+    private val doubleCharTokens = Arrays.asList("==", ">=", "<=", "*=", "++")
+    private val keywords = Arrays.asList("void", "int", "return", "unsigned", "long", "if")
 
 
-    private fun add(regex:Pattern, token:String) {
-        tokenInfos.add(TokenInfo(regex, token))
-    }
-    fun tokenize(str:String):ArrayList<Token> {
-        var s = str.trim({ it <= ' ' })
-        tokens = ArrayList()
-        while (s.isNotBlank())
-        {
-            var match = false
-            for (info in tokenInfos)
-            {
-                val m = info.regex.matcher(s)
-                if (m.find())
-                {
-                    match = true
-                    val tok = m.group().trim({ it <= ' ' })
-                    s = m.replaceFirst("").trim({ it <= ' ' })
-                    tokens.add(Token(info.token, tok))
-                    break
-                }
-            }
-            if (!match) throw Exception("Unexpected character in input: $s")
+
+    private fun getToken(strItr: ListIterator<Char>): Token? {
+        // Skip all spaces
+        while (isWhitespace(lastChar) && strItr.hasNext()) {
+            lastChar = strItr.next()
         }
-        return tokens
+        when {
+            isLetter(lastChar) -> {
+                var token = lastChar.toString()
+                while ({ lastChar = strItr.next(); isLetterOrDigit(lastChar) }()) {
+                    token += lastChar
+                }
+                val isToken = keywords.contains(token)
+
+                return Token((if (isToken) "Keyword" else "Identifier"), token)
+            }
+
+            isDigit(lastChar) -> {
+                var token = lastChar.toString()
+                while ({ lastChar = strItr.next(); isDigit(lastChar) }()) {
+                    token += lastChar
+                }
+                return Token("Constant", token)
+            }
+
+            singleCharTokens.contains(lastChar) -> {
+
+                val token = lastChar.toString()
+                lastChar = try {
+                    strItr.next()
+                } catch (e: NoSuchElementException) {
+                    0.toChar()
+                }
+
+                val doubleTok = token + lastChar
+                return if (doubleCharTokens.contains(doubleTok)) {
+                    val t = Token("Token", doubleTok)
+                    lastChar = strItr.next()
+                    t
+                } else {
+                    Token("Token", token)
+                }
+
+            }
+            lastChar == '"' -> {
+                var token = lastChar.toString()
+                try {
+                    while ({ lastChar = strItr.next(); lastChar !=  '"'}()) {
+                        token += lastChar
+                    }
+                } catch (e: NoSuchElementException) {}
+                token+= lastChar
+
+                lastChar = try {
+                    strItr.next()
+                } catch (e:NoSuchElementException) {
+                    ' '
+                }
+                return Token("String", token)
+            }
+
+            else -> {
+                return null
+            }
+        }
     }
-    
-    
-    init {
 
-        this.add(Pattern.compile("^([a-zA-Z]\\w*)"), "Identifier")
-
-        this.add(Pattern.compile("^(\\()"), "Open parenthesis")
-        this.add(Pattern.compile("^(\\))"), "Close parenthesis")
-
-        this.add(Pattern.compile("^(\\{)"), "Open brace")
-        this.add(Pattern.compile("^(})"), "Close brace")
-
-        this.add(Pattern.compile("^(;)"), "Semicolon")
-        this.add(Pattern.compile("^([=*])"), "Operator")
-
-        this.add(Pattern.compile("^(int)"), "Keyword")
-        this.add(Pattern.compile("^(return)"), "Keyword")
-
-        this.add(Pattern.compile("^([0-9]+)"), "Constant")
-
-        this.add(Pattern.compile("^(-)"), "Negation")
-        this.add(Pattern.compile("^(~)"), "Bitwise complement")
-        this.add(Pattern.compile("^(!)"), "Logical negation")
-
-//
+    fun tokenize(str: String): ArrayList<Token> {
+        tokens = ArrayList()
+        val strItr = str.toList().listIterator()
 
 
+        var tok: Token? = null
+
+        while ({ tok = getToken(strItr); tok }() != null) {
+            tokens.add(tok!!)
+        }
 
 
-
-
-
+        return tokens
     }
 }
--- src/test/kotlin/compiler/TokenizerTest.kt
+++ src/test/kotlin/compiler/TokenizerTest.kt
@@ -1,6 +1,7 @@
 package compiler
 
 import org.junit.Test
+
 import kotlin.test.assertEquals
 
 class TokenizerTest {
@@ -8,67 +9,162 @@ class TokenizerTest {
     fun simple(){
         val tokenizer = Tokenizer()
 
-        val code = """ void main() {     int answer = 6 * 7;     }   """
+        val code = """ void main() {     int answer = 6 * 7;   }   """
 
 
         val tokens = tokenizer.tokenize(code)
-        assertEquals("Identifier void\n" +
+        assertEquals("Keyword void\n" +
                 "Identifier main\n" +
-                "Open parenthesis (\n" +
-                "Close parenthesis )\n" +
-                "Open brace {\n" +
-                "Identifier int\n" +
+                "Token (\n" +
+                "Token )\n" +
+                "Token {\n" +
+                "Keyword int\n" +
                 "Identifier answer\n" +
-                "Operator =\n" +
+                "Token =\n" +
                 "Constant 6\n" +
-                "Operator *\n" +
+                "Token *\n" +
                 "Constant 7\n" +
-                "Semicolon ;\n" +
-                "Close brace }", tokens.joinToString("\n"))
+                "Token ;\n" +
+                "Token }", tokens.joinToString("\n"))
 
 
         val tokens2 = tokenizer.tokenize("int main() { return 2; }")
-        assertEquals("Identifier int\n" +
+        assertEquals("Keyword int\n" +
                 "Identifier main\n" +
-                "Open parenthesis (\n" +
-                "Close parenthesis )\n" +
-                "Open brace {\n" +
-                "Identifier return\n" +
+                "Token (\n" +
+                "Token )\n" +
+                "Token {\n" +
+                "Keyword return\n" +
                 "Constant 2\n" +
-                "Semicolon ;\n" +
-                "Close brace }", tokens2.joinToString("\n"))
+                "Token ;\n" +
+                "Token }", tokens2.joinToString("\n"))
 
     }
 
     @Test
     fun negotiation(){
         val tokenizer = Tokenizer()
-        assertEquals("Identifier int\n" +
+        assertEquals("Keyword int\n" +
                 "Identifier main\n" +
-                "Open parenthesis (\n" +
-                "Close parenthesis )\n" +
-                "Open brace {\n" +
-                "Identifier return\n" +
+                "Token (\n" +
+                "Token )\n" +
+                "Token {\n" +
+                "Keyword return\n" +
                 "Constant 1\n" +
-                "Negation -\n" +
+                "Token -\n" +
                 "Constant 3\n" +
-                "Close brace }",tokenizer.tokenize("int main() { return 1 - 3 }").joinToString("\n") )
+                "Token }",tokenizer.tokenize("int main() { return 1 - 3 }").joinToString("\n") )
     }
 
 
     @Test
-    fun BitwiseComplement(){
+    fun bitwiseComplement(){
         val tokenizer = Tokenizer()
-        assertEquals("Identifier int\n" +
+        assertEquals("Keyword int\n" +
                 "Identifier main\n" +
-                "Open parenthesis (\n" +
-                "Close parenthesis )\n" +
-                "Open brace {\n" +
-                "Identifier return\n" +
+                "Token (\n" +
+                "Token )\n" +
+                "Token {\n" +
+                "Keyword return\n" +
                 "Constant 1\n" +
-                "Bitwise complement ~\n" +
+                "Token ~\n" +
                 "Constant 3\n" +
-                "Close brace }",tokenizer.tokenize("int main() { return 1 ~ 3 }").joinToString("\n") )
+                "Token }",tokenizer.tokenize("int main() { return 1 ~ 3 }").joinToString("\n") )
+    }
+
+    @Test
+    fun factorial(){
+        val tokenizer = Tokenizer()
+        val factorialCode = """
+            int main()
+                {
+
+                    int n = 10;
+                    unsigned long long factorial = 1;
+
+
+
+
+                    if (n < 0)
+                        printf("Error! Factorial of a negative number doesn't exist.");
+
+                    else
+                    {
+                        for(int i=1; i<=n; ++i)
+                        {
+                            factorial *= i;
+                        }
+                        printf("Factorial of %d = %llu", n, factorial);
+                    }
+
+                    return 0;
+                }
+        """.trimIndent()
+        System.out.printf("factorialCode %s", factorialCode)
+        assertEquals("Keyword int\n" +
+                "Identifier main\n" +
+                "Token (\n" +
+                "Token )\n" +
+                "Token {\n" +
+                "Keyword int\n" +
+                "Identifier n\n" +
+                "Token =\n" +
+                "Constant 10\n" +
+                "Token ;\n" +
+                "Keyword unsigned\n" +
+                "Keyword long\n" +
+                "Keyword long\n" +
+                "Identifier factorial\n" +
+                "Token =\n" +
+                "Constant 1\n" +
+                "Token ;\n" +
+                "Keyword if\n" +
+                "Token (\n" +
+                "Identifier n\n" +
+                "Token <\n" +
+                "Constant 0\n" +
+                "Token )\n" +
+                "Identifier printf\n" +
+                "Token (\n" +
+                "String \"Error! Factorial of a negative number doesn't exist.\"\n" +
+                "Token )\n" +
+                "Token ;\n" +
+                "Identifier else\n" +
+                "Token {\n" +
+                "Identifier for\n" +
+                "Token (\n" +
+                "Keyword int\n" +
+                "Identifier i\n" +
+                "Token =\n" +
+                "Constant 1\n" +
+                "Token ;\n" +
+                "Identifier i\n" +
+                "Token <=\n" +
+                "Identifier n\n" +
+                "Token ;\n" +
+                "Token ++\n" +
+                "Identifier i\n" +
+                "Token )\n" +
+                "Token {\n" +
+                "Identifier factorial\n" +
+                "Token *=\n" +
+                "Identifier i\n" +
+                "Token ;\n" +
+                "Token }\n" +
+                "Identifier printf\n" +
+                "Token (\n" +
+                "String \"Factorial of %d = %llu\"\n" +
+                "Token ,\n" +
+                "Identifier n\n" +
+                "Token ,\n" +
+                "Identifier factorial\n" +
+                "Token )\n" +
+                "Token ;\n" +
+                "Token }\n" +
+                "Keyword return\n" +
+                "Constant 0\n" +
+                "Token ;\n" +
+                "Token }", tokenizer.tokenize(factorialCode).joinToString("\n"))
     }
 
 }
